2ccff2e5b7317e0aec94d24cfba90611bf8c3892
Prabhashi Hettiarachchi <63660305+Pubzzz@users.noreply.github.com>
2021-08-19 09:33:44 +0530

    update

--- CRMApp/src/components/peopleItem.js
+++ CRMApp/src/components/peopleItem.js
@@ -2,8 +2,8 @@ import React from "react";
 import { View,StyleSheet,Image,Text } from "react-native";
 import {connect} from 'react-redux';
 import {getTheme} from 'react-native-material-kit';
-import * as actions from '../actions';
-// import Icon from 'react-native-vector-icons/EvilIcons';
+// import * as actions from '../actions';
+import Icon from 'react-native-vector-icons/EvilIcons';
 
 const PeopleItem = (props)=>{
         return(
@@ -12,7 +12,7 @@ const PeopleItem = (props)=>{
                 source ={require('../images/background.jpg')}
                 style={styles.image}
                 />
-                {/* <Icon name={'user'} size={100} style={styles.icon} /> */}
+                <Icon name={'user'} size={100} style={styles.icon} />
                 <Text style={[theme.cardTitleStyle,styles.title]}>{props.people.firstName} {props.people.lastName}</Text>
                 <Text style={[theme.cardActionStyle,styles.action]}>{props.people.company}</Text>
             </View>
@@ -35,13 +35,13 @@ const styles = StyleSheet.create({
         backgroundColor:'black',
         color:'white',
     },
-    // icon:{
-    //     position:'absolute',
-    //     top:15,
-    //     left:0,
-    //     color:'white',
-    //     backgroundColor:'rgba(255,255,255,0)',
-    // }
+    icon:{
+        position:'absolute',
+        top:15,
+        left:0,
+        color:'white',
+        backgroundColor:'rgba(255,255,255,0)',
+    }
 })
 
 const theme = getTheme();
